3cb310f88f796338b0b116e79564cbd459d124d8
ranjeet-floyd <ranjeet.floyd@gmail.com>
2025-02-01 23:34:59 +0530

    intial project code with kannada flashcard explanation

---
+++ android/app/src/main/kotlin/com/example/flutter_kanada_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_kanada_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_kanada_app/flashcard.dart';
+import 'package:flutter_kanada_app/flashcard_speak.dart';
+
+
+void main() {
+  runApp(FlashCardApp());
+}
